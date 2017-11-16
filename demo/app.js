fea1834e1780adf3fc1e31eb71c0cd9904e7815b
Leonov Aleksandr <aleonov@mac173rds.rambler.ramblermedia.com>
2017-11-16 14:46:35 +0300

    fix demo app

--- demo/app.js
+++ demo/app.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
 import {
   SimpleCarousel,
   InfiniteCarousel,
   TabCarousel,
 } from './components';
 import s from './styles.scss';
-
-// injectTapEventPlugin();
 
 class App extends React.Component {
   render() {
